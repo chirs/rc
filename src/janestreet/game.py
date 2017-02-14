a4f2b91985550b34eb5d3b80e537d3a0409bb329
Christopher Edgemon <chrisedgemon@gmail.com>
2017-02-14 15:27:31 -0500

    minor fixes to jane street?

--- src/janestreet/game.py
+++ src/janestreet/game.py
@@ -1,3 +1,4 @@
+### https://www.janestreet.com/puzzles/current-puzzle/
 
 
 import random
@@ -9,47 +10,55 @@ move_cache = {}
 
 def play_round(n, previous=None, player=0):
 
-    opponent = (player + 1) % 2
-    
-    key = (n, previous)
+    key = (n, previous, player)
+
+    opponent = (player + 1) % 2    
+
     if key in cache:
         return cache[key]
 
     if n <= 9 and n != previous:
-        return True
+        move = n
+        result = True
     else:
-        possible_moves = [e for e in range(1, 9) if e != previous]
+        possible_moves = [e for e in range(1, 10) if e != previous]
         possible_results = [play_round(n - move, move, opponent) for move in possible_moves]
         winning_moves = [move for (move, result) in zip(possible_moves, possible_results) if result == False]
 
         if len(winning_moves) == 0:
-            if player == 0:
-                result = False
-            else:
-                result = True
-
+            move = None
+            result = False
         else:
             move = winning_moves[0]
-            #move_cache[key] = move
-            if player == 0:
-                result = True
-            else:
-                result = False
-            
+            result = True
 
-        cache[key] = result
-        return result
+    cache[key] = result
+    move_cache[key] = move
+    
+    return result
 
 
 def reconstruct(n):
     l = []
     previous = None
+    player = 0
 
-    while n != 0:
-        move = move_cache[(n, previous)]        
+    while True:
+        key = (n, previous, player)
+        if key not in move_cache:
+            return l
+
+        move = move_cache[key]
         l.append(move)
+
+        if move is None:
+            break
+        
         n -= move
         previous = move
+        player = (player + 1) % 2
+
+    return l
 
 
 def main():
@@ -59,14 +68,16 @@ def main():
     #print(play_round(10, None))
     #print(play_round(11, None))
 
-    #play_round(24, None)
-    #print(reconstruct(24))
+    n = 84
+    play_round(n, None)
+    print(reconstruct(n))
+    #import pdb; pdb.set_trace()
+
     
-    for e in range(100):
-        s = '%s: %s' % (e, play_round(e, None))
-        print(s)
+    #for e in range(100):
+    #    print('%s: %s' % (e, play_round(e)))
 
-
+        
 if __name__ == "__main__":
     main()
         
